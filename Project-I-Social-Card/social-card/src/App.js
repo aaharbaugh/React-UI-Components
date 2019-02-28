e804636fd6e6f135f6e7d33e82d6ab4e089b69df
aaharbaugh <aaronmharbaugh@gmail.com>
2019-02-27 22:15:49 -0600

    removed some things

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -9,13 +9,6 @@ const App = () => {
     <div className="container">
       <HeaderContainer />
       <CardContainer />
-
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
     </div>
   );
 };
